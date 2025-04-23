94158ce65d9d06e719abdc82d73c7acc48c00b10
fr0staman <mrfrostaman@gmail.com>
2025-04-23 20:09:24 +0300

    fix(mobile): monorepo react-native-cli integration, paths
    In this single commit you can see process to setup react-native-cli app to monorepo usage.

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        root: ["../.."],
+        alias: {
+          // define aliases to shorten the import paths
+          "@/ui": "../../packages/ui",
+        },
+        extensions: [".js", ".jsx", ".tsx", ".ios.js", ".android.js"],
+      },
+    ],
+  ],
 };
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const path = require("path");
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
 
 /**
  * Metro configuration
@@ -7,6 +11,16 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = getDefaultConfig(projectRoot);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+config.resolver.disableHierarchicalLookup = true;
+
+module.exports = config;
